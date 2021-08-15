249dc822a7a1adcc521523651a60501bd87eaf2d
matsushinDB11 <matsushin35@gmail.com>
2021-08-15 13:04:41 +0900

    add contloller/diaries.go to sample json response

--- src/contoroller/diaries.go
+++ src/contoroller/diaries.go
@@ -3,9 +3,21 @@ package contoroller
 import "github.com/gin-gonic/gin"
 
 func GetDiariesList(c *gin.Context) {
-
+	sort := c.Param("sort")
+	// sample response
+	c.JSON(200, gin.H{
+		"sample": sort,
+	})
 }
 
 func PostDiary(c *gin.Context) {
-
+	title := c.Param("title")
+	body := c.Param("body")
+	userid := c.Param("author_id")
+	// sample response
+	c.JSON(201, gin.H{
+		"title":  title,
+		"body":   body,
+		"userID": userid,
+	})
 }
