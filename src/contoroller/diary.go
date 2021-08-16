2a6a74cd371b4fab1388b9ac0aa24828b7b20e08
matsushinDB11 <matsushin35@gmail.com>
2021-08-16 11:44:50 +0900

    add sumple responses

--- src/contoroller/diary.go
+++ src/contoroller/diary.go
@@ -3,12 +3,31 @@ package contoroller
 import "github.com/gin-gonic/gin"
 
 func GetDiary(c *gin.Context) {
-
+	id := c.Param("id")
+	// sample response
+	c.JSON(200, gin.H{
+		"id":         id,
+		"authorID":   1111,
+		"title":      "志望企業が決められない",
+		"body":       "私はどこに向かっているのか",
+		"likedCount": 3,
+		"liked":      false,
+		"createdAt":  "2019-09-02T16:16:22+09:00",
+		"updatedAt":  "2021-09-04T16:16:22+09:00",
+	})
 }
 func PatchDiary(c *gin.Context) {
-
+	id := c.Param("id")
+	// sample response
+	c.JSON(200, gin.H{
+		"patch": id,
+	})
 }
 
 func DeleteDiary(c *gin.Context) {
-
+	id := c.Param("id")
+	// sample response
+	c.JSON(200, gin.H{
+		"delete": id,
+	})
 }
--- src/contoroller/diaryLikes.go
+++ src/contoroller/diaryLikes.go
@@ -3,9 +3,17 @@ package contoroller
 import "github.com/gin-gonic/gin"
 
 func PostDiaryLikes(c *gin.Context) {
-
+	id := c.Param("id")
+	// sample response
+	c.JSON(200, gin.H{
+		"liked": id,
+	})
 }
 
 func DeleteDiaryLikes(c *gin.Context) {
-
+	id := c.Param("id")
+	// sample response
+	c.JSON(200, gin.H{
+		"delete like": id,
+	})
 }
