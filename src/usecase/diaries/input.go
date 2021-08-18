866d4bf607b3ee3b3245b71e452370cd7edb4f3b
matsushinDB11 <matsushin35@gmail.com>
2021-08-18 17:48:31 +0900

    add usecase to struct & add to document

--- src/usecase/diaries/input.go
+++ src/usecase/diaries/input.go
@@ -1,4 +1,26 @@
 package diaries
 
+// Request Parameters
+
+type GetListInput struct {
+	Order string `json:"order" form:"order"`
+	Page  int    `json:"page" form:"page"`
+}
+
+type AddInput struct {
+	User  int    // domain.User
+	title string `json:"title" form:"title"`
+	Body  string `json:"body" form:"body"`
+}
+
 type GetInput struct {
+	id int `json:"id"`
+}
+
+type UpdateInput struct {
+	User   int    // domain.User
+	MemoID int    `validate:"gt=0"`
+	title  string `json:"title" form:"title"`
+	Body   string `json:"body" form:"body"`
+	// IsDeleted       bool `json:"isDeleted" form:"isDeleted"`
 }
