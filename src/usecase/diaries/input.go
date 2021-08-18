fd44075d2fe43fe76dd80ceb4d0276494ee1bc8c
matsushinDB11 <matsushin35@gmail.com>
2021-08-18 18:00:56 +0900

    add struct to usecase

--- src/usecase/diaries/input.go
+++ src/usecase/diaries/input.go
@@ -18,9 +18,24 @@ type GetInput struct {
 }
 
 type UpdateInput struct {
-	User   int    // domain.User
-	MemoID int    `validate:"gt=0"`
-	title  string `json:"title" form:"title"`
-	Body   string `json:"body" form:"body"`
+	User  int    // domain.User
+	ID    int    //`validate:"gt=0"`
+	title string `json:"title" form:"title"`
+	Body  string `json:"body" form:"body"`
 	// IsDeleted       bool `json:"isDeleted" form:"isDeleted"`
+}
+
+type DeleteInput struct {
+	ID   int `json:"id"`
+	User int
+}
+
+type Like struct {
+	ID   int `json:"id"`
+	User int
+}
+
+type DeLike struct {
+	ID   int `json:"id"`
+	User int
 }
